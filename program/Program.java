91974e8e938361b2177f1959499d1066b4717425
Luka Pavlic <luka.pavlic@gmail.com>
2025-11-12 20:15:42 +0100

    Tu notri je resitev ;)

--- program/Program.java
+++
@@ -1,9 +0,0 @@
-public class Program {
-
-    public static void main(String... args) {
-        System.out.println("Končni odgovor na vsa vprašanja je ");
-
-        //program zaženemo z java Program
-
-    }
-}
