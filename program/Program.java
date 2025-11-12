226477dd6b54fabc6e0bd9287dd12461645c9f8c
Luka Pavlic <luka.pavlic@gmail.com>
2025-11-12 20:14:51 +0100

    Brezvezen commit

---
+++ program/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+
+    public static void main(String... args) {
+        System.out.println("Končni odgovor na vsa vprašanja je ");
+
+        //program zaženemo z java Program
+
+    }
+}
